a2d077b24ed8215458eca29e3cab76f768b7ba2a
Vaishnavi47 <vaishnavigiradkar500@gmail.com>
2024-05-28 12:32:28 +0530

    login page basic structure

--- nimai-fe-code/src/Components/Login.js
+++ nimai-fe-code/src/Components/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Login() {
-  return <>Login</>;
+  const handleSubmit = () => {
+    alert("submited");
+  };
+  return (
+    <>
+      <h3>Login</h3>
+      <input type="text" id="login" />
+      <input type="password" id="password" />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </>
+  );
 }
 
 export default Login;
