017e5f97f393126db938df5738200a7242b56653
Vaishnavi47 <vaishnavigiradkar500@gmail.com>
2024-06-01 16:42:56 +0530

    lint

--- nimai-fe-code/src/Store/Reducers/loadingReducer.js
+++ nimai-fe-code/src/Store/Reducers/loadingReducer.js
@@ -1,10 +1,12 @@
+import { LOADING_FALSE, LOADING_TRUE } from "../Actions/actionTypes";
+
 const initialState = { isLoading: false };
 
 function loadingReducer(state = initialState, action) {
   switch (action.type) {
-    case "LOADING_TRUE":
+    case LOADING_TRUE:
       return { ...state, isLoading: true };
-    case "LOADING_FALSE":
+    case LOADING_FALSE:
       return { ...state, isLoading: false };
     default:
       return state;
